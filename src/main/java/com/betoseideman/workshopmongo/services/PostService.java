6bd0c4e6eb21341938fbb6826e6061f6a17256ee
Beto Seideman <betoseideman@gmail.com>
2022-01-24 18:54:54 -0300

    Obtendo post por id

---
+++ src/main/java/com/betoseideman/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.betoseideman.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.betoseideman.workshopmongo.domain.Post;
+import com.betoseideman.workshopmongo.repository.PostRepository;
+import com.betoseideman.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	PostRepository repo;
+	
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
